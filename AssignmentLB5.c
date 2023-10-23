5b3b1513cb18b7c28228fa51004a724b0f34c40a
snehalgaurav <147620139+snehalgaurav@users.noreply.github.com>
2023-10-23 13:53:26 +0530

    Add files via upload

---
+++ AssignmentLB5.c
@@ -0,0 +1,16 @@
+//Prommblem statement : Write a program which accept the name from user and print that name
+
+#include<stdio.h>
+
+int main()
+{
+    char Name[30];
+
+    printf("Please enter full name\n");
+    scanf("%c",Name);
+
+    printf("your name is %s",Name);
+    
+
+    return 0;
+}
